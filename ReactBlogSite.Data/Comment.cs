45f74b4b7682dfb248601fbf22dc4c0eba12fe1c
Yocheved Wajsbort <cheviwaj@gmail.com>
2022-05-22 13:25:14 -0400

    test

--- ReactBlogSite.Data/Comment.cs
+++ ReactBlogSite.Data/Comment.cs
@@ -6,7 +6,7 @@ namespace ReactBlogSite.Data
     public class Comment
     {
         public int ID { get; set; }
-        public string CommentatorName { get; set; }
+        public string CommentatorName { get; set; } //testing...
         public string Text { get; set; }
         public DateTime Date { get; set; }
         public int PostID { get; set; }
